513f0466b1d524f53093b70317a72ef1c1138144
Strikingwolf <Strikingwolf2012@gmail.com>
2014-07-26 09:50:55 -0500

    TRYING TO FIX
    m

--- src/main/java/com/strikingwolf/letsmodreboot/configuration/ConfigurationHandler.java
+++ src/main/java/com/strikingwolf/letsmodreboot/configuration/ConfigurationHandler.java
@@ -5,6 +5,26 @@ import net.minecraftforge.common.config.Configuration;
 
 public class ConfigurationHandler {
     public static void init(File configFile) {
-        Configuration configuration = new Configuration();
+
+        //create configuration object from the given file
+        Configuration configuration = new Configuration(configFile);
+        boolean configValue = false;
+        try
+        {
+            //load the configuration file
+            configuration.load();
+
+            //read in properties from configuration file
+            configValue = configuration.get("ForgeCraft", "configValue", true, "Example config value").getBoolean(true);
+        }
+        catch (Exception e)
+        {
+            //log the exception
+        }
+        finally {
+            //save the configuration file
+            configuration.save();
+         }
+        System.out.println("Configuration Test: " + configValue);
     }
 }
--- src/main/java/com/strikingwolf/letsmodreboot/reference/Reference.java
+++ src/main/java/com/strikingwolf/letsmodreboot/reference/Reference.java
@@ -6,6 +6,4 @@ public class Reference {
     public static final String VERSION = "1.7.10-1.0";
     public static final String CLIENT_PROXY_CLASS = "com.strikingwolf.letsmodreboot.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.strikingwolf.letsmodreboot.proxy.ServerProxy";
-
-
 }
