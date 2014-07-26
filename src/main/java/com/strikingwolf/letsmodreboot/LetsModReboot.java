572c469f0c8e45a367215c63aefc6b992267bef6
Strikingwolf <Strikingwolf2012@gmail.com>
2014-07-26 11:44:28 -0500

    Episode 7
    getting there

--- src/main/java/com/strikingwolf/letsmodreboot/LetsModReboot.java
+++ src/main/java/com/strikingwolf/letsmodreboot/LetsModReboot.java
@@ -1,15 +1,16 @@
 package com.strikingwolf.letsmodreboot;
 
-import com.strikingwolf.letsmodreboot.configuration.ConfigurationHandler;
+import com.strikingwolf.letsmodreboot.handler.ConfigurationHandler;
 import com.strikingwolf.letsmodreboot.proxy.IProxy;
 import com.strikingwolf.letsmodreboot.reference.Reference;
+import com.strikingwolf.letsmodreboot.utility.LogHelper;
 import cpw.mods.fml.common.Mod;
 import cpw.mods.fml.common.SidedProxy;
 import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION)
+@Mod(modid = Reference.MOD_ID, name = Reference.MOD_NAME, version = Reference.VERSION, guiFactory = Reference.GUI_FACTORY_CLASS)
 public class LetsModReboot
 {
     @Mod.Instance(Reference.MOD_ID)
@@ -22,17 +23,20 @@ public class LetsModReboot
     public void preInit(FMLPreInitializationEvent event)
     {
         ConfigurationHandler.init(event.getSuggestedConfigurationFile());
+        LogHelper.info("Pre Initialization Complete");
     }
 
     @Mod.EventHandler
     public void Init(FMLInitializationEvent event)
     {
+        LogHelper.info("Initialization Complete");
 
     }
 
     @Mod.EventHandler
     public void postInit(FMLPostInitializationEvent event)
     {
+        LogHelper.info("Post Initialization Complete");
 
     }
 
--- src/main/java/com/strikingwolf/letsmodreboot/reference/Reference.java
+++ src/main/java/com/strikingwolf/letsmodreboot/reference/Reference.java
@@ -6,4 +6,5 @@ public class Reference {
     public static final String VERSION = "1.7.10-1.0";
     public static final String CLIENT_PROXY_CLASS = "com.strikingwolf.letsmodreboot.proxy.ClientProxy";
     public static final String SERVER_PROXY_CLASS = "com.strikingwolf.letsmodreboot.proxy.ServerProxy";
+    public static final String GUI_FACTORY_CLASS = "com.strikingwolf.letsmodreboot.client.gui.GuiFactory";
 }
